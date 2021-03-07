3545a4d02171e67a4e23beb4288f6dc1c176f218
3to5thpower <nisi3genki@gmail.com>
2021-03-08 01:46:13 +0900

    :fire: delete unused function

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,6 @@ module Main where
 
 import Parse
 
-p' (Right []) = putStr "\n"
-p' (Right (result : rest)) = do
-  print result
-  p' $ Right rest
-p' (Left s) = putStrLn s
-
 main :: IO ()
 main = do
   s <- getContents
